6995eddb84b13767191cef5702cb145d40e2329a
林佳志乃 <twinklestaryoshino@gmail.com>
2018-10-10 00:12:34 +0900

    :+1: コメント追加

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -5,6 +5,12 @@ class RankingController < ApplicationController
 
   def ranking
     product_ids = Review.group(:product_id).order('count_product_id DESC').limit(5).count(:product_id).keys
+    # Review.group(:product_id)で、product_idごとにReviewのレコードがまとめられる。
+    # order('count_product_id DESC')で、product_idでまとめたレコードをレコード数でソート。
+    # count(:product_id)で、カラム名(product_id)とレコードの数のハッシュで返す。
+    # ActiveRecord::Relationクラスは複数のレコードの配列のような形のクラス。
+    # orderはActiveRecord::Rerationクラスのメソッドなのでハッシュには使えない。
+    # limitはActiveRecord::Rerationクラスのメソッドなのでハッシュには使えない。
     @ranking = product_ids.map { |id| Product.find(id) }
   end
 end
--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,24 +1,37 @@
 class Scraping
 
   def self.movie_urls
+    #まずself.movie_urlsメソッドが実行される。
     links = []
+    #linksというArrayクラスのインスタンス定義
     agent = Mechanize.new
+    #M3chanizeクラスのインスタンスを生成
     next_url = ""
+    #next_urlというstringクラスのインスタンス定義
 
     while true
+      # 次のページへ飛ぶためのURLを取得し、linksという配列に入れるためのwhile文
       current_page = agent.get("http://review-movie.herokuapp.com/" + next_url)
+      #http://review-movie.herokuapp.com/というページの情報を取得し、current_pageという変数に代入
+      #while文の２周目からはhttp://review-movie.herokuapp.com/にnext_url足したページ取得
       elements = current_page.search('.entry-title a')
+      #entry-titleというcssクラス配下のaタグ取得し、elementsという変数に代入
       elements.each do |ele|
         links << ele.get_attribute('href')
+        #elements(entry-title配下のaタグ)の中のhref属性の値を取得し、linksという配列に入れている
       end
 
       next_link = current_page.at('.pagenation .next a')
+      #pagenationというcssクラス配下のnextというcssクラス配下のaタグを取得し、next_linkという変数に代入
       break unless next_link
+      # next_linkがなかったらwhile文抜ける
       next_url = next_link.get_attribute('href')
+      # 先ほど取得したタグの中のhref属性の値をnext_urlという変数に代入
     end
 
     links.each do |link|
       get_product("http://review-movie.herokuapp.com/" + link)
+      # self.get_productメソッド実行。引数にページのURL持っていく
     end
   end
 
